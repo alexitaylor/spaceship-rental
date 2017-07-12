9a469cca2dc3442b5d7da68e2bd9bd6293ee99bc
alexitaylor <alexi@bluesunrise.com>
2017-07-12 16:03:55 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img class="logo" src="./assets/logo.png">
+      <hello></hello>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Home';
+
+export default {
+  components: {
+    Hello,
+  },
+};
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  text-align: center;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="row">
+      <div class="col s12">
+        <div class="card-panel teal">
+          <span class="white-text">Search Feature
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World!',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
