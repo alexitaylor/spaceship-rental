659cd8e2a232c6e349d9291059d0a751ce71535a
alexitaylor <alexi@bluesunrise.com>
2017-07-13 10:23:18 -0700

    Added spaceships dummy data

---
+++ src/data/spaceships.js
@@ -0,0 +1,67 @@
+/**
+ * Created by alexi on 7/13/17.
+ */
+export default {
+  spaceships: [
+    {
+      make: 'Space X',
+      model: 'Dragon',
+      description: 'Dragon is a reusable spacecraft developed by SpaceX, ' +
+      'an American private space transportation company based in Hawthorne, California.',
+      price: '$800,000',
+      passengers: '250',
+      destinations: ['Mars', 'San Francisco', 'Moon'],
+    },
+    {
+      make: 'Space X',
+      model: 'Dragon V2',
+      description: 'Dragon 2 is the second version of the SpaceX Dragon spacecraft, ' +
+      'which will be a human-rated vehicle able to make a terrestrial soft landing.',
+      price: '$600,000',
+      passengers: '1500',
+      destinations: ['Mars', 'San Francisco', 'Moon', 'New York'],
+    },
+    {
+      make: 'Space X',
+      model: 'Falcon Heavy',
+      description: 'Falcon Heavy, previously known as the Falcon 9 Heavy, ' +
+      'is a reusable super heavy lift space launch vehicle ',
+      price: '$1,000,000',
+      passengers: '500',
+      destinations: ['Mars', 'San Francisco', 'Moon', 'New York', 'Pluto'],
+    },
+    {
+      make: 'Millennium',
+      model: 'Falcon',
+      description: 'Despite her humble origins and shabby exterior, the Millennium Falcon has ' +
+      'played a role in some of the greatest victories of the Rebel Alliance and the New Republic.',
+      price: '$30,000',
+      passengers: '25',
+      destinations: ['New York', 'San Francisco', 'Wroclaw', 'Moon', 'Tatooine', 'Hooth', 'Naboo'],
+    },
+    {
+      make: 'Starship',
+      model: 'Enterprise',
+      description: 'Enterprise or USS Enterprise is the name of several fictional spacecraft, ' +
+      'some of which are the main craft and setting for various television series and films ' +
+      'in the Star Trek science fiction franchise.',
+      price: '$120,000',
+      passengers: '1,000',
+      destinations: ['New York', 'Wroclaw', 'Moon', 'Turkana IV', 'Nimbus III', 'Vulcan'],
+    },
+    {
+      make: 'Planet Express',
+      model: 'Ship',
+      description: 'The Planet Express Ship is a fictional anthropomorphic spaceship ' +
+      'in the animated series Futurama, which bears the official designation ' +
+      'U.S.S. Planet Express Ship.',
+      price: '$500',
+      passengers: '15',
+      destinations: [
+        'New York', 'Wroclaw', 'Moon',
+        'Mars', 'Tokyo', 'Amazonia', 'Doohan 6',
+        'Eternium', 'Kronos',
+      ],
+    },
+  ],
+};
