dcc059d813f95ac478e5107b805b1019f6c3dd87
alexitaylor <alexi@bluesunrise.com>
2017-07-14 18:15:11 -0700

    Added packages options
    Styling could be worked on.

--- src/components/Spaceship.vue
+++ src/components/Spaceship.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="hello">
+
     <div class="row">
       <div class="col s12"><h1><strong>{{ currentShip.make }}</strong> {{ currentShip.model }}</h1></div>
       <div class="col s6">
+
         <p>{{ currentShip.description }}</p>
         <h5>Price: {{ currentShip.priceUSD }} / ticket</h5>
         <h5>Passengers: {{ currentShip.passengers }}</h5>
@@ -10,15 +12,44 @@
           <li><strong>Destinations:</strong></li>
           <li v-for="place in currentShip.destinations">{{ place }}</li>
         </ul>
+
+        <div class="col s4">
+          <h5>Package</h5>
+        </div>
+        <div class="col s4">
+          <h5>Price</h5>
+        </div>
+        <div class="col s4">
+          <h5>Buy</h5>
+        </div>
+
+        <div v-for="item in packages" class="package">
+          <div class="col s4">
+            {{ item.package }}
+          </div>
+          <div class="col s4">
+            {{ item.price }}
+          </div>
+          <div class="col s4">
+            <a class="waves-effect waves-light btn">Buy</a>
+          </div>
+        </div>
+
       </div>
+
       <div class="col s6">
         <img :src="currentShip.img" width="200" height="300" />
       </div>
+
     </div>
+
+
+
     <div class="row">
       <div class="col s3 offset-s3"><a v-link="{ path: '/list' }" class="waves-effect waves-light btn">Back</a></div>
       <div class="col s3"><a class="waves-effect waves-light btn disabled">Book Spaceship</a></div>
     </div>
+
   </div>
 </template>
 
@@ -30,6 +61,11 @@
       return {
         msg: 'Hello Space Ship!',
         currentShip: {},
+        packages: [
+          { package: 'Luxury Class', price: '$20,000' },
+          { package: 'Business Class', price: '$40,000' },
+          { package: 'VIP', price: '$100,000' },
+        ],
       };
     },
     created() {
@@ -56,5 +92,9 @@
   li {
     display: inline-block;
     margin: 0 10px;
+  }
+
+  .btn {
+    margin-top: 5%;
   }
 </style>
